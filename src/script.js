1e291361e7c03d6ea0116a15ed8b939e290968a4
Lyumina-n <nedokosl@gmail.com>
2022-10-22 19:19:15 +0300

    Added current date and time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp){
+let date=new Date(timestamp);
+let days=[
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
+let day=days[date.getDay()];
+let hours=date.getHours();
+if(hours<10){
+    hours=`0${hours}`;
+};
+let minutes=date.getMinutes();
+if(minutes<10){
+    minutes=`0${minutes}`;
+};
+return`${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
@@ -10,8 +33,10 @@ let windElement=document.querySelector("#wind");
 windElement.innerHTML=Math.round(response.data.wind.speed);
 let humidityElement=document.querySelector("#humidity");
 humidityElement.innerHTML=response.data.temperature.humidity;
+let dateElement=document.querySelector("#date");
+dateElement.innerHTML=formatDate(response.data.time*1000);
 }
 
 let apiKey="3t24b911d0709b8ae0o92f53fd6c2444";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=Batumi&key=${apiKey}`;
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=Ericeira&key=${apiKey}`;
 axios.get(apiUrl).then(displayTemperature);
